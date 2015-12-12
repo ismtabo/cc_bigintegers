51ac057a2925d229d02254faec974dfd5620d6ec
Ismael Taboada Rodero <ismaeljose.taboada@alumnos.uva.es>
2015-12-12 17:54:55 +0100

    Update javadoc of collection.tree.node package.

--- app/src/collection/tree/node/NodeNumber.java
+++ app/src/collection/tree/node/NodeNumber.java
@@ -7,7 +7,7 @@ package collection.tree.node;
 import java.math.BigInteger;
 
 /**
- * Class that represents numbers in the tree.
+ * Class that represents literal numbers in the tree.
  */
 public class NodeNumber extends NodeExpression {
 
@@ -16,33 +16,52 @@ public class NodeNumber extends NodeExpression {
     private BigInteger value;
 
 
-
+    /**
+     * Initialize instance of class.
+     */
     public NodeNumber(){
         this(BigInteger.ZERO);
     }
 
 
-
+    /**
+     * Initialize instance of class given literal number value.
+     * 
+     * @param value {@link BigInteger} node literal.
+     */
     public NodeNumber(BigInteger value){
         super(null, null, null);
         this.value = value;
     }
 
 
-
+    /**
+     * Initialize instance of class given literal number expression.
+     * 
+     * @param value {@link String} node expression.
+     */
     public NodeNumber(String value){
         this(new BigInteger(value));
     }
 
 
-
+    /**
+     * Evaluate node value.
+     * 
+     * Evalutate node expression giving back the proper value.
+     * 
+     * @return literal number value.
+     * @throws IllegalArgumentException when variable does not exist in local storage {@link ExpressionTree#vars}.
+     */
     @Override
     public BigInteger operate() {
         return value;
     }
 
 
-
+    /**
+     * @return {@link String} expression of node.
+     */
     @Override
     public String toString() {
         return value.toString();
